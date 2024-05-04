be0f58f9a9814ace6d71b4b21d83c47c5a961015
IGOR <ozerosama@mail.ru>
2024-05-04 13:19:53 +0500

    1

--- app/src/main/java/com/example/dataseaapp/dataSea/MainActivity.kt
+++ app/src/main/java/com/example/dataseaapp/dataSea/MainActivity.kt
@@ -3,21 +3,35 @@ package com.example.dataseaapp.dataSea
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dataseaapp.CreateActivity
+import com.example.dataseaapp.dataSea.Model.DbHelper
 import com.example.dataseaapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var db:DbHelper
+    private lateinit var notesAdapter:NotesAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        db = DbHelper(this)
+        notesAdapter = NotesAdapter(db.getAllNotes(), this)
+
+        binding.notesRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.notesRecyclerView.adapter = notesAdapter
+
         binding.addButton.setOnClickListener {
             val intent = Intent(this, CreateActivity::class.java)
             startActivity(intent)
         }
+    }
+    override fun onResume(){
+        super.onResume()
+        notesAdapter.refreshData(db.getAllNotes())
     }
 }
